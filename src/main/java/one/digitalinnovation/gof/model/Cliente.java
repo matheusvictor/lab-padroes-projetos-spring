6087f31b3f3f01bbbb70bc847739e4511756e953
Matheus Victor <matheusvictor.salles@gmail.com>
2023-03-30 22:57:43 -0300

    refactor: Add notação Lombok
    - "Geração" de métodos acessores e construtores

--- src/main/java/one/digitalinnovation/gof/model/Cliente.java
+++ src/main/java/one/digitalinnovation/gof/model/Cliente.java
@@ -1,13 +1,16 @@
 package one.digitalinnovation.gof.model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.ManyToOne;
-import org.springframework.data.annotation.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Cliente {
 
     @Id
@@ -16,28 +19,4 @@ public class Cliente {
     private String nome;
     @ManyToOne
     private Endereco endereco;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public Endereco getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(Endereco endereco) {
-        this.endereco = endereco;
-    }
 }
